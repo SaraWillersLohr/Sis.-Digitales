742a75242db47799a4dcae096099e60f8363aa0a
Sara Willers <134223842+SaraWillersLohr@users.noreply.github.com>
2024-06-17 20:24:58 -0300

    Add files via upload

---
+++ pagina1.php
@@ -0,0 +1,14 @@
+
+<?php
+$semana[] ="lunes";
+$semana[] ="martes";
+$semana[] ="miencoles";
+$semana[] ="jueves";
+$semana[] ="viernes";
+$semana[] ="sabado";
+$semana[] ="domingo";
+
+echo "El primer dia de la semana es: ". $semana[0];
+echo "<br>";
+echo "El último dia de la semana es: ". $semana[6];
+?>
